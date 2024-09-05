3d17448c2063d01ed76e72a6926e5f65a2cbee1d
Attila Farago <attila.farago.hu@gmail.com>
2024-09-06 01:09:12 +0200

    houskeeping temp files

--- build/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("./index");
-console.log((0, index_1.hello)('world 122'));
-//# sourceMappingURL=app.js.map
