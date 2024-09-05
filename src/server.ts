4c837459ba14f8a5af09cdc60b11674275a8e475
Attila Farago <attila.farago.hu@gmail.com>
2024-09-06 01:07:22 +0200

    bugfix server code

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import fileUpload from 'express-fileupload';
-import { handleFileUpload } from './handlers';
+import fileUpload, { UploadedFile } from 'express-fileupload';
+import { Request, Response } from 'express';
+import { ProjectConverter } from './projectconverter';
 
 const app = express();
 const port = 3000;
@@ -13,6 +14,23 @@ app.use(fileUpload());
 
 // GET route
 // POST route for file upload
+export const handleFileUpload = async (req: Request, res: Response) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  try {
+    const file = req.files.file as UploadedFile;
+    const converter = new ProjectConverter();
+    const fileData = file.data as Buffer;
+    const retval = await converter.convert(fileData);
+
+    res.send(retval);
+  } catch (err) {
+    res.status(400).send(err);
+    //res.status(500).send('Error processing the zip file.');
+  }
+};
 app.post('/upload', handleFileUpload);
 
 // Start the server
